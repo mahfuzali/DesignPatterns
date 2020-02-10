b4235f5e36210cb81ef5d027b91f6939223d8c92
Mahfuz Ali <mahfuz.ali@hitachiconsulting.com>
2020-02-10 14:48:29 +0000

    Add project files.

---
+++ DesignPatterns/AbstractFactory/ProductionLine.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.AbstractFactory
+{
+    class ProductionLine
+    {
+    }
+
+    public interface IProductionLine
+    {
+        IVehicle CreateAVehicle();
+        IEngine CreateAEngine();
+    }
+
+    class JapaneseManufacturer : IProductionLine
+    {
+        public IEngine CreateAEngine()
+        {
+            return new TwoLiter();
+        }
+
+        public IVehicle CreateAVehicle()
+        {
+            return new Car();
+        }
+    }
+
+
+    class BritishManufacturer : IProductionLine
+    {
+        public IEngine CreateAEngine()
+        {
+            return new OneLiter();
+        }
+
+        public IVehicle CreateAVehicle()
+        {
+            return new Motorcycle();
+        }
+    }
+
+
+    public interface IVehicle
+    {
+        string Drive(IEngine engine);
+    }
+
+    class Car : IVehicle
+    {
+        public string Drive(IEngine engine)
+        {
+            return "This is a car";
+        }
+    }
+
+    class Motorcycle : IVehicle
+    {
+        public string Drive(IEngine engine)
+        {
+            return "This is a motorcycle";
+        }
+    }
+
+
+    public interface IEngine
+    {
+        string Start();
+    }
+
+    class OneLiter : IEngine
+    {
+        public string Start()
+        {
+            return "This is a one liter engine";
+        }
+    }
+
+    class TwoLiter : IEngine
+    {
+        public string Start()
+        {
+            return "This is a two liter engine";
+        }
+    }
+
+
+    class Client
+    {
+        public void Main()
+        {
+            ClientMethod(new JapaneseManufacturer());
+            Console.WriteLine();
+
+            ClientMethod(new BritishManufacturer());
+        }
+
+        public void ClientMethod(IProductionLine productionLine)
+        {
+            var car = productionLine.CreateAVehicle();
+            var engine = productionLine.CreateAEngine();
+
+            Console.WriteLine(engine.Start());
+            Console.WriteLine(car.Drive(engine));
+        }
+    }
+
+}
---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+    class ThreadSafeSingle
+    {
+        private ThreadSafeSingle() { }
+
+        private static ThreadSafeSingle _instance;
+
+        private static readonly object _lock = new object();
+
+        public static ThreadSafeSingle GetInstance(string value)
+        {
+
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ThreadSafeSingle();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public string Value { get; set; }
+    }
+}
